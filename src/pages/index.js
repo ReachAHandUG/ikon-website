7c540b5bff9b25e01f2e566d860af250d6bc6111
ReachAHand <107673924+ReachAHand@users.noreply.github.com>
2023-01-05 10:51:47 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9QCCDWH78V"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9QCCDWH78V');
+</script>
+
+
 import React, { useEffect } from "react"
 import { graphql } from "gatsby"
 import Meta from "../utilities/seo"

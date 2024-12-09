e24137f4de3dc317e1e79c1db39b93272158f1bc
ReachAHand <107673924+ReachAHand@users.noreply.github.com>
2024-12-09 10:15:17 +0300

    Update featured-video.js

--- src/components/home/featured-video.js
+++ src/components/home/featured-video.js
@@ -76,7 +76,8 @@ const Ele = () => {
         <Box>
             <Wrap>
                 <Video>
-                    <iframe width="560" height="315" src={'https://www.youtube.com/watch?v=luOIpkj21w4' + videoID + '?rel=0&amp;controls=0&amp;showinfo=0&amp;modestbranding=1'} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/luOIpkj21w4?si=H-TghXmmi7-U_dS3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </Video>
             </Wrap>
             <Strip />
